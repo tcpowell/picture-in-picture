6dd2914403054514f87af46df7f1c598530c1da1
dekuScrub731 <taylorcpowell@gmail.com>
2021-07-16 13:37:56 -0400

    Initial commit

---
+++ src/main/java/com/pip/PictureInPictureConfig.java
@@ -0,0 +1,168 @@
+package com.pip;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("pip")
+public interface PictureInPictureConfig extends Config
+{
+	enum quadrant
+	{
+		BOTTOM_RIGHT("Bottom Right", 4),
+		BOTTOM_LEFT("Bottom Left", 3),
+		TOP_LEFT("Top Left", 2),
+		TOP_RIGHT("Top Right", 1)
+		;
+
+		private String value;
+		private int id;
+
+		quadrant(String value, int id)
+		{
+			this.value = value;
+			this.id = id;
+		}
+
+		@Override
+		public String toString()
+		{
+			return this.value;
+		}
+
+		public int toInt()
+		{
+			return this.id;
+		}
+	}
+
+	enum targetSize
+	{
+		SMALL("320 x 180", 320, 180),
+		MEDIUM("480 x 270", 480, 270),
+		LARGE("640 x 360", 640, 360),
+		XLARGE("800 x 450", 800, 450)
+		;
+
+		private String value;
+		private int width;
+		private int height;
+
+		targetSize(String value, int width, int height)
+		{
+			this.value = value;
+			this.width = width;
+			this.height = height;
+		}
+
+		@Override
+		public String toString()
+		{
+			return this.value;
+		}
+		public int getHeight()
+		{
+			return this.height;
+		}
+		public int getWidth()
+		{
+			return this.width;
+		}
+	}
+
+	enum limitedDimension
+	{
+		HEIGHT("Height"),
+		WIDTH("Width")
+		;
+
+		private String value;
+
+		limitedDimension(String value)
+		{
+			this.value = value;
+		}
+
+		@Override
+		public String toString()
+		{
+			return this.value;
+		}
+
+	}
+
+	enum clickAction
+	{
+		FOCUS("Focus RuneLite", 0),
+		NOTHING("Do Nothing", 1)
+		;
+
+		private String value;
+		private int action;
+
+		clickAction(String value, int action)
+		{
+			this.action = action;
+			this.value = value;
+		}
+
+		@Override
+		public String toString()
+		{
+			return this.value;
+		}
+		public int clickMode()
+		{
+			return this.action;
+		}
+	}
+
+	@ConfigItem(
+			keyName = "quadrantID",
+			name = "Position",
+			description = "Configures the position of the Picture in Picture",
+			position = 0
+	)
+	default quadrant quadrantID() { return quadrant.BOTTOM_RIGHT; }
+
+	@ConfigItem(
+			keyName = "paddingX",
+			name = "Horizontal Padding",
+			description = "The horizontal padding (in pixels) from the left/right edge of the screen",
+			position = 1
+	)
+	default int paddingX() { return 40; }
+
+	@ConfigItem(
+			keyName = "paddingY",
+			name = "Vertical Padding",
+			description = "The vertical padding (in pixels) from the top/bottom edge of the screen",
+			position = 2
+	)
+	default int paddingY() { return 25; }
+
+	@ConfigItem(
+			keyName = "targetSize",
+			name = "Target Size",
+			description = "Specifies the target size of the Picture in Picture",
+			position = 3
+	)
+	default targetSize targetSize() { return targetSize.MEDIUM; }
+
+	@ConfigItem(
+			keyName = "limitedDimension",
+			name = "Limited Dimension",
+			description = "Configures which dimension is limited when not 16:9",
+			position = 4
+	)
+	default limitedDimension limitedDimension() { return limitedDimension.HEIGHT; }
+
+	@ConfigItem(
+			keyName = "clickAction",
+			name = "Click Action",
+			description = "Action to perform when the Picture in Picture is clicked",
+			position = 5
+	)
+	default clickAction clickAction() { return clickAction.FOCUS; }
+
+}
---
+++ src/main/java/com/pip/PictureInPicturePlugin.java
@@ -0,0 +1,242 @@
+package com.pip;
+
+import com.google.inject.Provides;
+
+import javax.imageio.ImageIO;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.GameState;
+import net.runelite.api.events.ClientTick;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientUI;
+import net.runelite.client.ui.DrawManager;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.function.Consumer;
+import javax.swing.*;
+
+
+@Slf4j
+@PluginDescriptor(
+		name = "Picture In Picture",
+		description = "Displays picture in picture mode when RuneLite is not in focus",
+		tags = {"pip", "picture", "display", "afk"}
+)
+public class PictureInPicturePlugin extends Plugin
+{
+
+	private static boolean focused = true;
+	private static boolean pipUp = false;
+	private JFrame pipFrame = null;
+	private JLabel lbl = null;
+	private int pipWidth, pipHeight;
+	private double pipScale;
+
+	@Inject
+	private ClientUI clientUi;
+
+	@Inject
+	private ScheduledExecutorService executor;
+
+	@Inject
+	private DrawManager drawManager;
+
+	@Inject
+	private PictureInPictureConfig config;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		log.debug("PIP started!");
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		log.debug("PIP stopped!");
+		destroyPip();
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		if (gameStateChanged.getGameState() == GameState.LOGIN_SCREEN)
+			destroyPip();
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick tick)
+	{
+		if (!focused) {
+			Window window = javax.swing.FocusManager.getCurrentManager().getActiveWindow();
+			if (window == null) {
+				if (pipFrame == null) {
+					initializePip();
+					log.debug("PIP initialized");
+				}
+			}
+		}
+	}
+
+	@Subscribe
+	public void onClientTick(ClientTick event)
+	{
+		if (focused != clientUi.isFocused()) {
+			focused = clientUi.isFocused();
+			if (focused)
+				destroyPip();
+		}
+		if (!focused) {
+			if (pipFrame != null) {
+				if (pipUp) {
+					updatePip();
+					//log.debug("PIP updated");
+				}
+			}
+		}
+	}
+
+	@Provides
+	PictureInPictureConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(PictureInPictureConfig.class);
+	}
+
+	public void startPip(Image screenshot) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				if (config.limitedDimension().toString().equals("Width")) {
+					pipWidth = config.targetSize().getWidth();
+					pipScale = (double) pipWidth / (double) screenshot.getWidth(null) ;
+					pipHeight = (int) (screenshot.getHeight(null) * pipScale);
+				}
+				else {
+					pipHeight = config.targetSize().getHeight();
+					pipScale = (double) pipHeight / (double) screenshot.getHeight(null);
+					pipWidth = (int) (screenshot.getWidth(null) * pipScale);
+				}
+
+				Image img = screenshot.getScaledInstance(pipWidth, pipHeight, Image.SCALE_FAST);
+				ImageIcon icon=new ImageIcon(img);
+
+				pipFrame=new JFrame();
+				pipFrame.setFocusableWindowState(false);
+				pipFrame.setType(Window.Type.UTILITY);
+				pipFrame.setLayout(new FlowLayout());
+				pipFrame.setSize(img.getWidth(null),img.getHeight(null));
+
+				lbl=new JLabel();
+				lbl.setIcon(icon);
+
+				pipFrame.setUndecorated(true);
+				pipFrame.add(lbl);
+				pipFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				pipFrame.setAlwaysOnTop(true);
+
+				pipFrame.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent e) {
+						pipClicked();
+					}
+				});
+
+				//get screen info
+				GraphicsConfiguration gc = clientUi.getGraphicsConfiguration();
+				Rectangle bounds = gc.getBounds();
+				Insets screenInsets = Toolkit.getDefaultToolkit().getScreenInsets(gc);
+				Rectangle effectiveScreenArea = new Rectangle();
+				effectiveScreenArea.x = bounds.x + screenInsets.left;
+				effectiveScreenArea.y = bounds.y + screenInsets.top;
+				effectiveScreenArea.height = bounds.height - screenInsets.top - screenInsets.bottom;
+				effectiveScreenArea.width = bounds.width - screenInsets.left - screenInsets.right;
+
+				//set location
+				if (config.quadrantID().toInt() == 1)
+					pipFrame.setLocation(effectiveScreenArea.width - pipWidth - config.paddingX(),config.paddingY());
+				else if (config.quadrantID().toInt() == 2)
+					pipFrame.setLocation(config.paddingX(),config.paddingY());
+				else if (config.quadrantID().toInt() == 3)
+					pipFrame.setLocation(config.paddingX(),effectiveScreenArea.height - pipHeight - config.paddingY());
+				else
+					pipFrame.setLocation(effectiveScreenArea.width - pipWidth - config.paddingX(),effectiveScreenArea.height - pipHeight - config.paddingY());
+
+				// Display the window.
+				pipFrame.pack();
+				pipFrame.setVisible(true);
+				pipUp = true;
+			}
+		});
+
+	}
+
+	public void destroyPip() {
+		if (pipFrame != null) {
+			pipFrame.setVisible(false);
+			pipFrame.dispose();
+			pipFrame = null;
+			pipUp = false;
+		}
+	}
+
+	public void pipClicked() {
+		log.debug("PIP Clicked");
+		if (config.clickAction().clickMode() == 0) {
+			destroyPip();
+			clientUi.forceFocus();
+		}
+	}
+
+	//runs first to initialize pip
+	public void initializePip() {
+		Consumer<Image> imageCallback = (img) ->
+		{
+			executor.submit(() -> startPip(img));
+		};
+		drawManager.requestNextFrameListener(imageCallback);
+	}
+
+	//updates if pip is already up
+	public void updatePip() {
+		Consumer<Image> imageCallback = (img) ->
+		{
+			executor.submit(() -> updatePip(img));
+		};
+		drawManager.requestNextFrameListener(imageCallback);
+	}
+
+	//update image
+	public void updatePip (Image image) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Image img = image.getScaledInstance(pipWidth, pipHeight, Image.SCALE_FAST);
+				ImageIcon icon = new ImageIcon(img);
+				icon.getImage().flush();
+				lbl.setIcon(icon);
+			}
+		});
+	}
+
+	//test only (take screenshot)
+	public void takeScreenshot(Image image) {
+		BufferedImage screenshot = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		Graphics graphics = screenshot.getGraphics();
+		graphics.drawImage(image, 0, 0, null);
+
+		File f = new File("clientImage.png");
+		try {
+			ImageIO.write(screenshot, "PNG", f);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
