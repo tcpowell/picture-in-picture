50fc7adc15c8dea995559bd59e7365ba223e90c1
dekuScrub731 <taylorcpowell@gmail.com>
2021-08-12 22:55:38 -0400

    Added configurable options for status bars

--- src/main/java/com/pip/PictureInPictureConfig.java
+++ src/main/java/com/pip/PictureInPictureConfig.java
@@ -1,8 +1,11 @@
 package com.pip;
 
+import net.runelite.api.Skill;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
+
 import java.awt.RenderingHints;
 
 @ConfigGroup("pip")
@@ -188,11 +191,75 @@ public interface PictureInPictureConfig extends Config
 
 	}
 
+	enum barType
+	{
+		HITPOINTS("Hitpoints", Skill.HITPOINTS),
+		PRAYER("Prayer", Skill.PRAYER),
+		NONE("Disabled", null)
+		;
+
+		private String value;
+		private Skill skill;
+
+		barType(String value, Skill skill)
+		{
+			this.value = value;
+			this.skill = skill;
+		}
+
+		@Override
+		public String toString()
+		{
+			return this.value;
+		}
+		public Skill getSkill()
+		{
+			return this.skill;
+		}
+	}
+
+	enum barPosition
+	{
+		BEFORE("Before", 0),
+		AFTER("After", 1),
+		OUTSIDE("Outside", 2)
+		;
+
+		private String value;
+		private int position;
+
+		barPosition(String value, int position)
+		{
+			this.value = value;
+			this.position = position;
+		}
+
+		@Override
+		public String toString()
+		{
+			return this.value;
+		}
+		public int getPosition()
+		{
+			return this.position;
+		}
+	}
+
+	// =========== Picture in Picture Settings Section ===========
+
+	@ConfigSection(
+			name = "Picture in Picture Settings",
+			description = "Size, quality, and position settings for the picture in picture window",
+			position = 0
+	)
+	String pictureInPictureSettings = "pictureInPictureSettings";
+
 	@ConfigItem(
 			keyName = "quadrantID",
 			name = "Position",
 			description = "Configures the position of the Picture in Picture",
-			position = 0
+			position = 0,
+			section = pictureInPictureSettings
 	)
 	default quadrant quadrantID() { return quadrant.BOTTOM_RIGHT; }
 
@@ -200,7 +267,8 @@ public interface PictureInPictureConfig extends Config
 			keyName = "redrawRate",
 			name = "Redraw Rate",
 			description = "Configures the redraw rate of the Picture in Picture",
-			position = 1
+			position = 1,
+			section = pictureInPictureSettings
 	)
 	default redrawRate redrawRate() { return redrawRate.STANDARD; }
 
@@ -208,7 +276,8 @@ public interface PictureInPictureConfig extends Config
 			keyName = "renderQuality",
 			name = "Render Quality",
 			description = "Configures the render quality of the Picture in Picture",
-			position = 2
+			position = 2,
+			section = pictureInPictureSettings
 	)
 	default renderQuality renderQuality() { return renderQuality.MEDIUM; }
 
@@ -216,7 +285,8 @@ public interface PictureInPictureConfig extends Config
 			keyName = "paddingX",
 			name = "Horizontal Padding",
 			description = "The horizontal padding (in pixels) from the left/right edge of the screen",
-			position = 3
+			position = 3,
+			section = pictureInPictureSettings
 	)
 	default int paddingX() { return 40; }
 
@@ -224,7 +294,8 @@ public interface PictureInPictureConfig extends Config
 			keyName = "paddingY",
 			name = "Vertical Padding",
 			description = "The vertical padding (in pixels) from the top/bottom edge of the screen",
-			position = 4
+			position = 4,
+			section = pictureInPictureSettings
 	)
 	default int paddingY() { return 25; }
 
@@ -232,7 +303,8 @@ public interface PictureInPictureConfig extends Config
 			keyName = "targetSize",
 			name = "Target Size",
 			description = "Specifies the target size of the Picture in Picture",
-			position = 5
+			position = 5,
+			section = pictureInPictureSettings
 	)
 	default targetSize targetSize() { return targetSize.MEDIUM; }
 
@@ -240,7 +312,8 @@ public interface PictureInPictureConfig extends Config
 			keyName = "limitedDimension",
 			name = "Limited Dimension",
 			description = "Configures which dimension is limited when not 16:9",
-			position = 6
+			position = 6,
+			section = pictureInPictureSettings
 	)
 	default limitedDimension limitedDimension() { return limitedDimension.HEIGHT; }
 
@@ -248,8 +321,55 @@ public interface PictureInPictureConfig extends Config
 			keyName = "clickAction",
 			name = "Click Action",
 			description = "Action to perform when the Picture in Picture is clicked",
-			position = 7
+			position = 7,
+			section = pictureInPictureSettings
 	)
 	default clickAction clickAction() { return clickAction.REQUEST; }
+
+
+	// =========== Status Bar Section ===========
+
+	@ConfigSection(
+			name = "Status Bars",
+			description = "Status Bar settings",
+			position = 1
+	)
+	String statusBarSettings = "statusBarSettings";
+
+	@ConfigItem(
+			keyName = "leftBar",
+			name = "Left Bar",
+			description = "Configures the left status bar",
+			position = 0,
+			section = statusBarSettings
+	)
+	default barType leftBar() { return barType.HITPOINTS; }
+
+	@ConfigItem(
+			keyName = "rightBar",
+			name = "Right Bar",
+			description = "Configures the right status bar",
+			position = 1,
+			section = statusBarSettings
+	)
+	default barType rightBar() { return barType.PRAYER; }
+
+	@ConfigItem(
+			keyName = "barPosition",
+			name = "Bar Position",
+			description = "Sets the position of the status bars relative to the picture in picture",
+			position = 2,
+			section = statusBarSettings
+	)
+	default barPosition barPosition() { return barPosition.OUTSIDE; }
+
+	@ConfigItem(
+			keyName = "barText",
+			name = "Show Counters",
+			description = "Shows current value of the status on the bar",
+			position = 3,
+			section = statusBarSettings
+	)
+	default boolean barText() { return true; }
 
 }
--- src/main/java/com/pip/PictureInPicturePlugin.java
+++ src/main/java/com/pip/PictureInPicturePlugin.java
@@ -3,7 +3,7 @@ package com.pip;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
@@ -13,6 +13,8 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientUI;
 import net.runelite.client.ui.DrawManager;
+import net.runelite.client.ui.FontManager;
+
 import java.awt.*;
 import java.awt.image.*;
 import java.awt.event.*;
@@ -33,12 +35,101 @@ public class PictureInPicturePlugin extends Plugin
 	private static boolean pipUp = false;
 	private JFrame pipFrame = null;
 	private JLabel lbl = null;
+	private pipBar leftBar, rightBar;
+
 	private int clientTick = 0;
 	private int pipWidth, pipHeight;
 	private double pipScale;
+	private int maxHealth, currentHealth, maxPrayer, currentPrayer;
+	private Color[] healthColor;
+	private static final int GAP = 3;
+	private static final int BAR_WIDTH = 20;
+
+	Skill leftSkill, rightSkill;
+
+	private static final Color PRAYER_COLOR = new Color(32, 160, 160);
+	private static final Color PRAYER_BG_COLOR = new Color(10, 50, 50);
+	private static final Color[] PRAYER = {PRAYER_COLOR, PRAYER_BG_COLOR};
+
+	private static final Color HEALTH_COLOR = new Color(160, 32, 0);
+	private static final Color HEALTH_BG_COLOR = new Color(50, 10, 0);
+	private static final Color[] HEALTH = {HEALTH_COLOR, HEALTH_BG_COLOR};
+
+	private static final Color POISONED_COLOR = new Color(0, 160, 0);
+	private static final Color POISONED_BG_COLOR = new Color(0, 50, 0);
+	private static final Color[] POISONED = {POISONED_COLOR, POISONED_BG_COLOR};
+
+	private static final Color VENOMED_COLOR = new Color(0, 90, 0);
+	private static final Color VENOMED_BG_COLOR = new Color(0, 25, 0);
+	private static final Color[] VENOMED = {VENOMED_COLOR, VENOMED_BG_COLOR};
+
+	private static final Color DISEASE_COLOR = new Color(200, 160, 64);
+	private static final Color DISEASE_BG_COLOR = new Color(63, 50, 20);
+	private static final Color[] DISEASE = {DISEASE_COLOR, DISEASE_BG_COLOR};
+
+	private class pipBar extends JPanel {
+
+		int maxLevel;
+		int currentLevel;
+		Color[] colors;
+		int barWidth;
+
+		private pipBar(int maxLevel, int currentLevel, Color[] colors, int barWidth) {
+			this.maxLevel = maxLevel;
+			this.currentLevel = currentLevel;
+			this.colors = colors;
+			this.barWidth = barWidth;
+			setFont(FontManager.getRunescapeSmallFont());
+		}
+
+		private void updateBar(int maxLevel, int currentLevel, Color[] colors, int barWidth) {
+			this.maxLevel = maxLevel;
+			this.currentLevel = currentLevel;
+			this.colors = colors;
+			this.barWidth = barWidth;
+			repaint();
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(colors[0]);
+			g.fillRect(0,0,barWidth,pipHeight);
+			int bgHeight = (int) ((maxLevel - currentLevel)*pipHeight/((float) maxLevel));
+			g.setColor(colors[1]);
+			g.fillRect(0, 0, barWidth, bgHeight);
+
+			if (config.barText()) {
+				g.setFont(FontManager.getRunescapeSmallFont());
+
+				String text = String.valueOf(currentLevel);
+				int y = 20;
+				int x = BAR_WIDTH / 2 - g.getFontMetrics().stringWidth(text) / 2;
+
+				//text outline
+				g.setColor(Color.BLACK);
+				g.drawString(text, x, y + 1);
+				g.drawString(text, x, y - 1);
+				g.drawString(text, x + 1, y);
+				g.drawString(text, x - 1, y);
+
+				//text
+				g.setColor(Color.WHITE);
+				g.drawString(text, x, y);
+			}
+		}
+
+		@Override
+		public Dimension getPreferredSize() {
+			return new Dimension(20, pipHeight); // appropriate constants
+		}
+	}
 
 	@Inject
 	private ClientUI clientUi;
+
+	@Inject
+	private Client client;
 
 	@Inject
 	private ScheduledExecutorService executor;
@@ -76,6 +167,8 @@ public class PictureInPicturePlugin extends Plugin
 			Window window = javax.swing.FocusManager.getCurrentManager().getActiveWindow();
 			if (window == null) {
 				if (pipFrame == null) {
+					updateHitpoints();
+					updatePrayer();
 					initializePip();
 					log.debug("PIP initialized");
 				}
@@ -104,6 +197,17 @@ public class PictureInPicturePlugin extends Plugin
 			}
 		}
 		clientTick++;
+
+		//split this from the pip (bars can update more frequently if needed
+		if (!focused) {
+			if (pipFrame != null) {
+				if (pipUp) {
+					updateHitpoints();
+					updatePrayer();
+					updateBars();
+				}
+			}
+		}
 	}
 
 	@Provides
@@ -112,7 +216,31 @@ public class PictureInPicturePlugin extends Plugin
 		return configManager.getConfig(PictureInPictureConfig.class);
 	}
 
+	private void updateHitpoints(){
+		currentHealth = client.getBoostedSkillLevel(Skill.HITPOINTS);
+		maxHealth = client.getRealSkillLevel(Skill.HITPOINTS);
+		healthColor = HEALTH;
+		int poisonState = client.getVar(VarPlayer.IS_POISONED);
+		if (poisonState >= 1000000)
+			healthColor = VENOMED;
+		else if (poisonState > 0)
+			healthColor = POISONED;
+		else if (client.getVar(VarPlayer.DISEASE_VALUE) > 0)
+			healthColor = DISEASE;
+	}
+	private void updatePrayer(){
+		currentPrayer = client.getBoostedSkillLevel(Skill.PRAYER);
+		maxPrayer = client.getRealSkillLevel(Skill.PRAYER);
+	}
+
 	private void startPip(Image image) {
+
+		int position = config.barPosition().getPosition();
+		leftSkill = config.leftBar().getSkill();
+		rightSkill = config.rightBar().getSkill();
+
+		final int offset = ((leftSkill != null) ? GAP + BAR_WIDTH : 0) + ((rightSkill != null) ? GAP + BAR_WIDTH : 0);
+
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -133,14 +261,45 @@ public class PictureInPicturePlugin extends Plugin
 				pipFrame=new JFrame();
 				pipFrame.setFocusableWindowState(false);
 				pipFrame.setType(Window.Type.UTILITY);
-				pipFrame.setLayout(new FlowLayout());
-				pipFrame.setSize(img.getWidth(null),img.getHeight(null));
-
+				pipFrame.setLayout(new FlowLayout(FlowLayout.LEFT, GAP, GAP));
+				pipFrame.setSize(img.getWidth(null)+offset,img.getHeight(null));
 				lbl=new JLabel();
 				lbl.setIcon(icon);
-
 				pipFrame.setUndecorated(true);
-				pipFrame.add(lbl);
+
+				//pull in bar info from config
+				if (leftSkill == Skill.HITPOINTS)
+					leftBar = new pipBar(maxHealth, currentHealth, healthColor, BAR_WIDTH);
+				else if (leftSkill == Skill.PRAYER)
+					leftBar = new pipBar(maxPrayer, currentPrayer, PRAYER, BAR_WIDTH);
+				if (rightSkill == Skill.HITPOINTS)
+					rightBar = new pipBar(maxHealth, currentHealth, healthColor, BAR_WIDTH);
+				else if (rightSkill == Skill.PRAYER)
+					rightBar = new pipBar(maxPrayer, currentPrayer, PRAYER, BAR_WIDTH);
+
+				//set the order of bars and pip window
+				if (position == 0) {
+					if (leftSkill != null)
+						pipFrame.add(leftBar);
+					if (rightSkill != null)
+						pipFrame.add(rightBar);
+					pipFrame.add(lbl);
+				}
+				else if (position ==1) {
+					pipFrame.add(lbl);
+					if (leftSkill != null)
+						pipFrame.add(leftBar);
+					if (rightSkill != null)
+						pipFrame.add(rightBar);
+				}
+				else {
+					if (leftSkill != null)
+						pipFrame.add(leftBar);
+					pipFrame.add(lbl);
+					if (rightSkill != null)
+						pipFrame.add(rightBar);
+				}
+
 				pipFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 				pipFrame.setAlwaysOnTop(true);
 
@@ -162,13 +321,13 @@ public class PictureInPicturePlugin extends Plugin
 
 				//set location
 				if (config.quadrantID().toInt() == 1)
-					pipFrame.setLocation(effectiveScreenArea.width - pipWidth - config.paddingX(),config.paddingY());
+					pipFrame.setLocation(effectiveScreenArea.width - pipWidth - config.paddingX() - offset,config.paddingY());
 				else if (config.quadrantID().toInt() == 2)
 					pipFrame.setLocation(config.paddingX(),config.paddingY());
 				else if (config.quadrantID().toInt() == 3)
 					pipFrame.setLocation(config.paddingX(),effectiveScreenArea.height - pipHeight - config.paddingY());
 				else
-					pipFrame.setLocation(effectiveScreenArea.width - pipWidth - config.paddingX(),effectiveScreenArea.height - pipHeight - config.paddingY());
+					pipFrame.setLocation(effectiveScreenArea.width - pipWidth - config.paddingX() - offset,effectiveScreenArea.height - pipHeight - config.paddingY());
 
 				// Display the window.
 				pipFrame.pack();
@@ -226,6 +385,23 @@ public class PictureInPicturePlugin extends Plugin
 				ImageIcon icon = new ImageIcon(img);
 				icon.getImage().flush();
 				lbl.setIcon(icon);
+			}
+		});
+	}
+
+	//update bars
+	private void updateBars () {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				if (leftSkill == Skill.HITPOINTS)
+					leftBar.updateBar(maxHealth, currentHealth, healthColor, BAR_WIDTH);
+				else if (leftSkill == Skill.PRAYER)
+					leftBar.updateBar(maxPrayer, currentPrayer, PRAYER, BAR_WIDTH);
+				if (rightSkill == Skill.HITPOINTS)
+					rightBar.updateBar(maxHealth, currentHealth, healthColor, BAR_WIDTH);
+				else if (rightSkill == Skill.PRAYER)
+					rightBar.updateBar(maxPrayer, currentPrayer, PRAYER, BAR_WIDTH);
 			}
 		});
 	}
